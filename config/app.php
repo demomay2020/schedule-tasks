89fb22e41d20173f8c72caf077dd64a0fa6f5792
Tahir Khan <codewaretech@gmail.com>
2020-05-07 13:56:35 +0530

    app url changed

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://schedule-tasks-may-2020.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
