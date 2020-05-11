85fc4652be877286eec144ade1db9ab21012e294
Tahir Khan <codewaretech@gmail.com>
2020-05-11 07:35:56 +0530

    htaccess file changed

--- app/Http/Controllers/SchedulerController.php
+++ app/Http/Controllers/SchedulerController.php
@@ -18,7 +18,7 @@ class SchedulerController extends Controller
     public function __construct() {
         
         //$cu_route = Route::getCurrentRoute()->getActionName();
-        $cu_route=array('0'=>'aaaa','1'=>'display');
+        $cu_route='aaaa@display';
         $route_details = explode('@',$cu_route);        
         $action=$route_details[1];       
         if($action == "display" && Auth::guest()){
