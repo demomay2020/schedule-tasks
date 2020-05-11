8cf48caa139d38a7177cbaa7de0196dba30788ba
Tahir Khan <codewaretech@gmail.com>
2020-05-11 08:33:01 +0530

    htaccess file changed

--- resources/views/schedulers/step2.blade.php
+++ resources/views/schedulers/step2.blade.php
@@ -51,7 +51,7 @@
                 startDate: new Date(), 
                 daysOfWeekDisabled: [0,6],
             }).on('changeDate', function(e) {
-                
+                alert(2222);
                 var curdate = e.format('yyyy-mm-dd');
                 $('#chosen_date').val(curdate);
                 $('#schedule_area').html('');
@@ -59,7 +59,7 @@
                 $('#schedule_area').html('Loading.....');
                 $('#schedule_area').show('slow');
                 $('#continueDiv').hide();
-                $.get("{{route('schedule.check',['date']=>curdate)}}",
+                $.get("/schedule/check/" + curdate,
                 {
                   
                 },
