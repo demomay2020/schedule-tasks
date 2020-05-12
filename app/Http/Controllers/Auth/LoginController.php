cb9f93e88c12f92c5de1fa6e2807a6953f7fd967
Tahir Khan <codewaretech@gmail.com>
2020-05-12 11:08:06 +0530

    htaccess file changed

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,10 +36,4 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-    
-    protected function loggedOut(Request $request)
-    {
-        return redirect('/login');
-    }
-    
 }
